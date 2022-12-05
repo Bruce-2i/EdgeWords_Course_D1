e1c02d82671aca8f4726d42017957244eccf1a22
Bruce Devlin <Bruce.Devlin@2itesting.com>
2022-12-05 13:59:07 +0000

    Day 1 Complete

---
+++ src/test/java/HelloWebDriver.java
@@ -0,0 +1,110 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class HelloWebDriver {
+    /**<p>Variable to store the WebDriver.</p>*/
+    WebDriver webDriver;
+
+    /**<p>The WebDriver Navigation.</p>*/
+    WebDriver.Navigation navigate;
+
+    /**<p>Quit and dispose of the WebDriver after each test.</p>*/
+    @AfterEach
+    void webDriverCleanup() {
+        webDriver.quit();
+        webDriver = null;
+    }
+
+    /**<p>Setup the WebDriver before each test.</p>*/
+    @BeforeEach
+    void webDriverSetup() {
+        //Create a new Chrome WebDriver.
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+    /**
+     * <p>Login to an authenticated web-page using credentials.</p>
+     * @throws Exception The user details provided where refused by the web-page.
+     */
+    @Test
+    void loginToAuthPage() throws Exception {
+        //Set up the variables used in the test.
+        String url = "https://www.edgewordstraining.co.uk/webdriver2/" ;
+        String username = "edgewords";
+        String password = "edgewords123";
+
+        //Navigate to page.
+        navigate = webDriver.navigate();
+        navigate.to(url);
+
+        //Click on login page button.
+        webDriver.findElement(By.linkText("Login To Restricted Area")).click();
+
+        //Enter Login details
+        webDriver.findElement(By.id("username")).sendKeys(username);
+        webDriver.findElement(By.id("password")).sendKeys(password);
+
+        //Submit the details.
+        webDriver.findElement(By.linkText("Submit")).click();
+
+        //Instantiate the detailsAccepted bool.
+        boolean detailsAccepted = true;
+
+        //w.Until(ExpectedConditions.alertIsPresent()) creates an exception when it times-out...
+        //... searching for an alert.
+        //It's best to suppress this exception as we won't always see the exception.
+        try
+        {
+            //Wait 2 seconds for page to verify details.
+            WebDriverWait w = new WebDriverWait(webDriver, Duration.ofSeconds(2));
+
+            //If there is an alert present...
+            if(w.until(ExpectedConditions.alertIsPresent()) != null)
+            {
+                detailsAccepted = false;
+            }
+        }
+        catch (Exception exception)
+        {
+            //I must put a catch here, idk what to do with it...
+        }
+
+        //Check if the details where refused.
+        if (!detailsAccepted)
+        {
+            //Fail the test if the detail are incorrect.
+            throw new Exception("Failed to Login using details provided.");
+        }
+    }
+
+    /**<p>Drags a slider on a Web-page.</p>*/
+    @Test
+    void dragSlider() {
+        //Navigate to page.
+        navigate = webDriver.navigate();
+        navigate.to("https://www.edgewordstraining.co.uk/webdriver2/docs/cssXPath.html");
+
+        //Create Gripper.
+        WebElement gripper = webDriver.findElement(By.cssSelector(".ui-slider-handle"));
+
+        //Create the Action.
+        Actions actions = new Actions(webDriver);
+        Action dragDrop = actions.moveToElement(gripper)
+                .clickAndHold().moveByOffset(100, 0)
+                .release().build();
+        //Execute Action.
+        dragDrop.perform();
+    }
+}
